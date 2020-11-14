9c7a82564d402af66428cc0c6835b816e04bf87a
KhamzinaOO <71382148+KhamzinaOO@users.noreply.github.com>
2020-11-14 16:07:00 +0500

    menu-screen

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FillwordMenu
+{
+    class Program
+    {
+        private static string[] Menu()
+        {
+            string[] menu = new string[5];
+            menu[0] = "       Игра <<Филворды>>";
+            menu[1] = "          новая игра";
+            menu[2] = "          продолжить";
+            menu[3] = "           рейтинг";
+            menu[4] = "            выход";
+            return menu;
+        }
+        static void Main(string[] args)
+        {
+            string[] menu = Menu();
+            for (int i=0;i<menu.Length;i++)
+            {
+                Console.WriteLine(menu[i]);
+            }
+        }
+    }
+}
